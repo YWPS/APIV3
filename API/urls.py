189570ea97e40c2a7a8d6f82e6bffdfef324942c
Kasper-Liu <65485161+Kasper-Liu@users.noreply.github.com>
2021-06-22 11:00:48 +0800

    Delete API directory

--- API/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path('product/create/<name>', CreateProduct),
-    path('product/get/<hash>', GetProduct),
-    path('image/create/<name>', CreateImage),
-    path('image/get/<name>', GetImage),
-]
